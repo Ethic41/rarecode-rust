65a53e220f5b1f8736acffd0892eb2dc1b0a9dfe
ethic41 <dahirmuhammad3@gmail.com>
2025-07-22 08:11:53 +0100

    completed sum-evens exercise

---
+++ sum-evens/src/main.rs
@@ -0,0 +1,26 @@
+/* -=-<[ Bismillahirrahmanirrahim ]>-=-
+ -*- coding: utf-8 -*-
+ @Date    : 2025-07-22 08:03:05
+ @Author  : Dahir Muhammad Dahir (dahirmuhammad3@gmail.com)
+ @About   : Sum of Evens
+*/
+
+fn main() {
+    let start: u32 = 0;
+    let end: u32 = 5;
+
+    let result: u32 = sum_evens(start, end);
+    println!("{}", result);
+}
+
+pub fn sum_evens(start: u32, end: u32) -> u32 {
+    let mut sum: u32 = 0;
+
+    for _i in start..end {
+        if _i % 2 == 0 {
+            sum += _i
+        }
+    }
+
+    sum
+}
