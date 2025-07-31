66512ab1f820654d35796595514ec298461452b2
ethic41 <dahirmuhammad3@gmail.com>
2025-07-31 10:16:59 +0100

    feat: add is_in_vector and k_is_at_idx functions for vector element checks

--- rust-ownership-2/src/main.rs
+++ rust-ownership-2/src/main.rs
@@ -1,3 +1,5 @@
+use std::ops::Index;
+
 fn main() {
     let x: i32 = 2;
     let y: i32 = inc(x);
@@ -8,4 +10,18 @@ fn main() {
 
 pub fn inc(x: i32) -> i32 {
     x + 1
+}
+
+pub fn is_in_vector(v: &Vec<i32>, k: i32) -> bool {
+    v.contains(&k)
+}
+
+pub fn k_is_at_idx(v: &Vec<i32>, k: i32, idx: usize) -> bool{
+    for i in 0..v.len(){
+        if i == idx && v[i] == k {
+            return true;
+        }
+    }
+
+    false
 }
