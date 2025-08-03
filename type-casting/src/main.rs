eb4562ad76d58afde431e066dd1ebe93ade3b8c1
ethic41 <dahirmuhammad3@gmail.com>
2025-08-03 10:52:20 +0100

    feat: add initial Cargo.toml and Cargo.lock files; implement main.rs with a basic structure

---
+++ type-casting/src/main.rs
@@ -0,0 +1,10 @@
+/* -=-<[ Bismillahirrahmanirrahim ]>-=-
+ -*- coding: utf-8 -*-
+ @Date    : 2025-08-03 10:51:56
+ @Author  : Dahir Muhammad Dahir (dahirmuhammad3@gmail.com)
+ @About   : Type Casting Exercises
+*/
+
+fn main() {
+    println!("Hello, world!");
+}
