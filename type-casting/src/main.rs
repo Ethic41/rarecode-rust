82ca151f16f665a6ba87c64c9de203f99172e497
ethic41 <dahirmuhammad3@gmail.com>
2025-08-03 11:24:00 +0100

    feat: implement max, absolute_value, absolute_value_1, foo, and find functions for basic numeric operations

--- type-casting/src/main.rs
+++ type-casting/src/main.rs
@@ -8,3 +8,34 @@
 fn main() {
     println!("Hello, world!");
 }
+
+pub fn max(x: i32, y: i32) -> i32 {
+    let m: i32 = if x > y { x } else { y };
+
+    m
+}
+
+pub fn absolute_value(x: i32) -> i32 {
+    let ans: i32 = if x < 0 { x * -1 } else { x };
+
+    ans
+}
+
+pub fn absolute_value_1(x: i32) -> u32 {
+    let ans: i32 = if x < 0 { x * -1 } else { x };
+
+    ans as u32
+}
+
+pub fn foo(x: i32) -> u32 {
+    x as u32
+}
+
+pub fn find(v: &Vec<i32>, k: i32) -> i32 {
+	for i in 0..v.len() {
+	    if k == v[i] {
+	        return i as i32;
+	    }
+	}
+	-1
+}
