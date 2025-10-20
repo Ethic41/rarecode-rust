6a03fc94bfa9c90308fac9314bbe0bbb1821c653
ethic41 <dahirmuhammad3@gmail.com>
2025-10-20 11:21:55 +0100

    feat: implement largest_set function to find the largest HashSet in a vector

--- collection-practice/src/main.rs
+++ collection-practice/src/main.rs
@@ -16,3 +16,20 @@ fn main() {
 pub fn size_of_set(s: &HashSet<i32>) -> usize {
     s.len()
 }
+
+pub fn largest_set(v: &Vec<HashSet<i32>>) -> usize {
+    let len: usize = v.len();
+    if len < 1 {
+        return len;
+    }
+
+    let mut largest: usize = 0;
+
+    for i in 0..len {
+        if v[i].len() > largest {
+            largest = v[i].len(); 
+        }
+    }
+    
+    largest
+}
